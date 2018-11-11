ac39c752468bc347bd012905a3a731adc7baf923
TehPers <tehperz@gmail.com>
2018-11-11 04:22:10 -0600

    Added a bunch of helper classes

---
+++ Conflux/Convenience/EnumerableExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Joueur.cs.Conflux.Convenience {
+    public static class EnumerableExtensions {
+
+        public static IEnumerable<T> Yield<T>(this T source) {
+            yield return source;
+        }
+    }
+}
---
+++ Conflux/Responsive/WhereResponsiveOperator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Joueur.cs.Conflux.Responsive {
+    internal class WhereResponsiveOperator<T> : ResponsiveValue<T> {
+        public WhereResponsiveOperator(ResponsiveValue<T> source, Func<T, bool> predicate) : base(predicate(source.Value) ? source.Value : default) {
+            source.ValueChanged += newValue => {
+                if (predicate(newValue)) {
+                    this.Value = newValue;
+                }
+            };
+        }
+    }
+}
